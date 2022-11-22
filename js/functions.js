a98301ac3b4908b74dc9eb820faa7fd36e5fbaee
Luke Calderon <luke.a.calderon@gmail.com>
2022-11-22 12:15:51 -0600

    worked on excersises attempted 2nd to last question followed along with lesson and tried other methods

--- js/functions.js
+++ js/functions.js
@@ -74,12 +74,25 @@ console.log(random);
  * > calculateTip(0.25, 25.50) // returns 6.375
  * > calculateTip(0.15, 33.42) // returns 5.013
  */
-
-function calculateTip(bill, tip){
-    let totalTip = (bill * tip)
-    console.log(totalTip)
+let tipAnswer = calculateTip(1412, 14);
+console.log(tipAnswer);
+function calculateTip(total, tip){
+    let tipConversion = tip / 100; // 0.20, 0.30
+    let answer = tipConversion * total;
+    // console.log(total, tip);
+    // let tipFormatted = "0." + tip;
+    // tipFormatted = parseFloat(tipFormatted);
+    // console.log(tipFormatted)
+    // let answer = total * tipFormatted;
+    alert(answer);
 }
-calculateTip(50, 0.18);
+
+
+// function calculateTip(bill, tip){
+//     let totalTip = (bill * tip)
+//     console.log(totalTip)
+// }
+// calculateTip(50, 0.18);
 
 /**
  * TODO:
@@ -87,8 +100,25 @@ calculateTip(50, 0.18);
  * prompt the user for the bill total and a percentage they would like to tip,
  * then display the dollar amount they should tip
  */
-let bill = prompt('Total bill amount?');
-alert(calculateTip(bill));
+
+const userBill = prompt("What is the total of the bill?");
+const userTip = prompt("What is your tip?");
+
+const conversionOfBillToNumber = parseInt(userBill); // this will convert the string from the prompt to a number
+const conversionOfUserTipToNumber = parseInt(userTip);
+
+const callToFunction = calculateTip(conversionOfBillToNumber, conversionOfUserTipToNumber); // these are the arguments for the function
+console.log(callToFunction);
+
+
+// let bill = prompt('Total bill amount?');
+//
+// let tip = prompt("Tip percentage?");
+
+// //calculateTip(bill, tip);
+//
+// alert("Thanks for the " + calculateTip(bill, tip) + " tip!")
+
 /**
  * TODO:
  * Create a function named `applyDiscount`. This function should accept a price
@@ -103,3 +133,23 @@ alert(calculateTip(bill));
  *
  * > applyDiscount(45.99, 0.12) // 40.4712
  */
+var originalPrice = 100;
+var discountPercent = 20; // 20%
+let appliedDiscount = applyDiscount(originalPrice, discountPercent) // 80
+console.log("about to apply a " + discountPercent + "% discount to an original price of " + originalPrice)
+console.log(appliedDiscount);
+
+function applyDiscount(price, discount){
+    let discountFormatted = discount / 100;
+    let result = (1 - discountFormatted) * price;
+    return result.toFixed(2);
+}
+
+
+//arrow Function below
+
+// const sayHello = (name) => {
+//
+// }
+
+//let message = `Hello ${name}!`    backitck concatenation method
