f1a8d8636a2801ae4f1f3075fd31ff05d7dfffce
Luke Calderon <luke.a.calderon@gmail.com>
2022-12-01 15:28:26 -0600

    j

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,29 +1,27 @@
 "use strict";
 
-// Number to skip is: 27
-//
-// Here is an odd number: 1
-// Here is an odd number: 3
-// Here is an odd number: 5
-// Here is an odd number: 7
-// Here is an odd number: 9
-// Here is an odd number: 11
-// Here is an odd number: 13
-// Here is an odd number: 15
-// Here is an odd number: 17
-// Here is an odd number: 19
-// Here is an odd number: 21
-// Here is an odd number: 23
-// Here is an odd number: 25
-// Yikes! Skipping number: 27
-// Here is an odd number: 29
-// Here is an odd number: 31
-// Here is an odd number: 33
-// Here is an odd number: 35
-// Here is an odd number: 37
-// Here is an odd number: 39
-// Here is an odd number: 41
-// Here is an odd number: 43
-// Here is an odd number: 45
-// Here is an odd number: 47
-// Here is an odd number: 49
+
+// 2
+// while(true){
+//     let userNum = prompt("Enter an odd number between 1 and 50");
+//     userNum = parseFloat(userNum);
+//     if (userNum % 2 === 1 && userNum <= 50 && userNum >= 1){
+//         break;
+//     }
+// }
+
+//3
+while(true){
+    let userNum = prompt("Enter an odd number between 1 and 50");
+    userNum = parseFloat(userNum);
+    if (userNum % 2 === 1 && userNum <= 50 && userNum >= 1){
+        for(let i = 1; i <= 50; i += 2){
+            if(i === userNum){
+                console.log(`Yikes!! skipping number ${i} `)
+            } else{
+                console.log(i);
+            }
+        }
+        break;
+    }
+}
