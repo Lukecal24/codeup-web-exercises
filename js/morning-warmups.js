866b0994fe95b5b925fb9dfc48051fd8c79ead88
Luke Calderon <luke.a.calderon@gmail.com>
2023-01-18 09:50:37 -0600

    this works

--- js/morning-warmups.js
+++ js/morning-warmups.js
@@ -178,10 +178,10 @@
 
 // This loop should print the numbers from 0 through 9 in the console
 
-for (let i = 0; i < 10; i++) {
-    console.log("The value of i is " + i);
-    // i++;
-}
+// for (let i = 0; i < 10; i++) {
+//     console.log("The value of i is " + i);
+//     // i++;
+// }
 
 // desired output : 0, 1 ,2,  3, 4, 5, 6, 7, 8, 9
 
@@ -217,9 +217,85 @@ for (let i = 0; i < 10; i++) {
 // var clothingToWear = isColdOutside ? 'shorts and a t-shirt' : 'sweater and a jacket';
 
 
-console.log(10 % 3);
+// console.log(10 % 3);
+//
+//
+// var array = ['John', 'Paul', 'George', 'Ringo', 'Santana'];
 
 
-var array = ['John', 'Paul', 'George', 'Ringo', 'Santana'];
+// ================================= WARM UP
+
+// Write a function that takes a neighborhood object and determines if it is desirable. A neighborhood is desirable if the median home price is less than 300000, crime rates are low, and the total rating of schools is at least 24.
+
+// example data...
+
+const neighborhood1 = {
+    neighborhood: "Lovely Estates",
+    medianHomePrice: 280000,
+    pool: true,
+    tennis: false,
+    crimeRate: "low",
+    schools: [
+        {name: "Elementary School", rating: 8},
+        {name: "Middle School", rating: 6},
+        {name: "High School", rating: 8}
+    ]
+};
+
+const neighborhood2 = {
+    neighborhood: "Luminous Estates",
+    medianHomePrice: 270000,
+    pool: true,
+    tennis: false,
+    crimeRate: "high",
+    schools: [
+        {name: "Elementary School", rating: 8},
+        {name: "Middle School", rating: 8},
+        {name: "High School", rating: 8}
+    ]
+}
+
+const neighborhood3 = {
+    neighborhood: "Oak Mountain",
+    medianHomePrice: 290000,
+    pool: false,
+    tennis: false,
+    crimeRate: "low",
+    schools: [
+        {name: "Elementary School", rating: 8},
+        {name: "Middle School", rating: 8},
+        {name: "High School", rating: 8}
+    ]
+}
+
+const neighborhood4 = {
+    neighborhood: "Ginormous Acres",
+    medianHomePrice: 350000,
+    pool: true,
+    tennis: true,
+    crimeRate: "low",
+    schools: [
+        {name: "Elementary School", rating: 9},
+        {name: "Middle School", rating: 9},
+        {name: "High School", rating: 9}
+    ]
+}
+
+function goodArea (input) {
+   let schoolGrade = 0;
+   for(let i = 0; i < input.length; i++);
+   if (input.medianHomePrice <= 300000 && input.crimeRate === 'low' && schoolGrade == 24){
+       return true;
+   } else{
+       return false;
+   }
+}
+
+console.log(goodArea(neighborhood3));
+
+// fn(neighborhood1) // returns false due to school rating
+// fn(neighborhood2) // returns false due to crime rate
+// fn(neighborhood3) // returns true
+// fn(neighborhood4) // returns false due to median home price
 
 
